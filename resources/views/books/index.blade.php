75f586f4a84638a167aca1ab16c4121215d477b7
Albert Kelamn Sitorus <albertkelmans@gmail.com>
2026-09-13 14:10:14 +0700

    Implement basic MVC structure

---
+++ resources/views/books/index.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Daftar Buku</title>
+</head>
+<body>
+    <h1>Daftar Buku</h1>
+    <p>Sistem Informasi Perpustakaan</p>
+</body>
+</html>
---
+++ resources/views/members/index.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Daftar Member</title>
+</head>
+<body>
+    <h1>Daftar Member</h1>
+    <p>Sistem Informasi Perpustakaan</p>
+</body>
+</html>
